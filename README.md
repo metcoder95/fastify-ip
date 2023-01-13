203fd89240e0a2fbcffc0bde7269aa03d1f00923
Carlos Fuentes <me@metcoder.dev>
2023-01-13 23:33:00 +0100

    feat!: v1 (#1)

--- README.md
+++ README.md
@@ -1,2 +1,115 @@
-# lib_template
-Template repository for bootstrap new libraries faster
+# fastify-ip
+
+---
+
+`fastify-racing` is a plugin which allows to infer the incoming request's IP based on a custom subset of well known headers used by different providers or technologies that possible sits in-front of your fastify application.
+
+## How it works?
+
+The plugin will make a best-effort to infer the request's IP based on a subset of well-known headers, which looks as follows:
+```
+'x-client-ip' // Most common
+'x-forwarded-for' // Mostly used by proxies
+'cf-connecting-ip' // Cloudflare
+'Cf-Pseudo-IPv4' // Cloudflare
+'fastly-client-ip'
+'true-client-ip' // Akamai and Cloudflare
+'x-real-ip' // Nginx
+'x-cluser-client-ip' // Rackspace LB
+'forwarded-for'
+'x-forwarded'
+'forwarded'
+'x-appengine-user-ip' // GCP App Engine
+```
+
+The plugin will use a FIFO strategy to infer the right IP. This can be customised by passing a custom  order  property that includes your custom headers.
+
+>Note: It is important to remark that this does not alters the `Request#ips` behaviour for inferring IPs when setting the `FastifyOptions#trustProxy` to `true`. It rather allows you to infer the IP of a given request by headers out of the common spec or standard.
+
+## Setup
+
+Install by running `npm install fastify-ip`.
+
+Then register the plugin to your fastify instance:
+
+```js
+const fastify = require('fastify')({
+  logger: true
+})
+
+fastify.register(require('fastify-racing'), {
+    order: ['x-my-ip-header'],
+    strict: false
+})
+```
+
+### Options
+
+- `order` - `string[] | string` - **optional**: Array of custom headers or single custom header to be appended to the prior list of well-known headers. The headers passed will be prepend to the default headers list. It can also be used to alter the order of the list as deduplication of header names is made while loading the plugin.
+
+- `strict` - `boolean` - **optional**: Indicates whether to override the default list of well-known headers and replace it with the header(s) passed through the `order` option. If set to `true` without `order` property being provided, will not take any effect on the plugin. Default `false`.
+
+
+### API
+
+The plugin will decorate the Request object with a set of utils that can be handy for managing IPs.
+
+- `isIP(pseudo: string): boolean` - It will return `true` if a given string is a valid IPv4 or IPv6, or `false` otherwise.
+- `isIPv4(pseudo: string): boolean` - Similar to `isIP` but will validate of the given string is a valid `IPv4`.
+- `isIPv6(pseudo: string): boolean` - Similar to `isIP` but will validate of the given string is a valid `IPv6`.
+- `inferIPVersion(pseudo: string): 0 | 4 | 6` - It will try to infer the IPv of a given IP, returning `4` for `IPv4` and `6` for `IPv6`, will return `0` if the given string does not match any of the IPv.
+
+## How to use it?
+
+**JavaScript**
+
+```js
+app.get('/', (req, reply) => {
+    req.log.info({ ip: req.ip }, 'my ip!')
+    req.log.info({ isValid: req.isIP('hello!') } /* false */)
+    req.log.info({ isIPv4: req.isIPv4('127.0.0.1') })
+    req.log.info({ isIPv6: req.isIPv6('::1') })
+    req.log.info({ version: req.inferIPVersion('127.0.0.1') /* 4 */ })
+    req.log.info({ version: req.inferIPVersion('::1') /* 6 */ })
+    req.log.info({ version: req.inferIPVersion('asd') /* 0 */ })
+
+    reply.send(req.ip)
+})
+```
+
+**TypeScript**
+
+```ts
+app.post('/', (request: FastifyRequest, reply: FastifyReply) => {
+    req.log.info({ ip: req.ip }, 'my ip!')
+    req.log.info({ isValid: req.isIP('hello!') } /* false */)
+    req.log.info({ isIPv4: req.isIPv4('127.0.0.1') })
+    req.log.info({ isIPv6: req.isIPv6('::1') })
+    req.log.info({ version: req.inferIPVersion('127.0.0.1') /* 4 */ })
+    req.log.info({ version: req.inferIPVersion('::1') /* 6 */ })
+    req.log.info({ version: req.inferIPVersion('asd') /* 0 */ })
+
+    reply.send(req.ip)
+});
+```
+
+## Type Definitions
+
+```ts
+export interface FastifyIPOptions {
+  order?: string[] | string;
+  strict?: boolean;
+}
+
+declare module 'fastify' {
+  interface FastifyRequest {
+    isIP(pseudo: string): boolean;
+    isIPv4(pseudo: string): boolean;
+    isIPv6(pseudo: string): boolean;
+    inferIPVersion(pseudo: string): 0 | 4 | 6;
+  }
+}
+```
+
+
+> See [test](test/index.test.js) for more examples.
--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,342 @@
 'use strict'
+const { faker } = require('@faker-js/faker')
+const tap = require('tap')
+const fastify = require('fastify')
+
+const plugin = require('../')
+
+tap.plan(2)
+
+tap.test('Plugin#Decoration', scope => {
+  scope.plan(5)
+
+  scope.test('Should decorate the request', async t => {
+    const app = fastify()
+
+    app.register(plugin)
+
+    app.get('/', (req, reply) => {
+      t.type(req.isIP, 'function')
+      t.type(req.isIPv4, 'function')
+      t.type(req.isIPv6, 'function')
+      t.type(req.inferIPVersion, 'function')
+      t.type(req._fastifyip, 'string')
+
+      reply.send('')
+    })
+
+    t.plan(5)
+
+    await app.inject('/')
+  })
+
+  scope.test('#isIP - Should return boolean', async t => {
+    const app = fastify()
+
+    app.register(plugin)
+
+    app.get('/', (req, reply) => {
+      t.ok(req.isIP(faker.internet.ipv4()))
+      t.ok(req.isIP(faker.internet.ipv6()))
+      t.notOk(req.isIP('xyz'))
+
+      reply.send('')
+    })
+
+    t.plan(3)
+
+    await app.inject({
+      path: '/'
+    })
+  })
+
+  scope.test('#isIPv4 - Should return boolean', async t => {
+    const app = fastify()
+
+    app.register(plugin)
+
+    app.get('/', (req, reply) => {
+      t.ok(req.isIPv4(faker.internet.ipv4()))
+      t.notOk(req.isIPv4(faker.internet.ipv6()))
+      t.notOk(req.isIPv4('xyz'))
+
+      reply.send('')
+    })
+
+    t.plan(3)
+
+    await app.inject({
+      path: '/'
+    })
+  })
+
+  scope.test('#isIPv6 - Should return boolean IP', async t => {
+    const app = fastify()
+
+    app.register(plugin)
+
+    app.get('/', (req, reply) => {
+      t.ok(req.isIPv6(faker.internet.ipv6()))
+      t.notOk(req.isIPv6(faker.internet.ipv4()))
+      t.notOk(req.isIPv6('xyz'))
+
+      reply.send('')
+    })
+
+    t.plan(3)
+
+    await app.inject({
+      path: '/'
+    })
+  })
+
+  scope.test('#inferIPVersion - Should return boolean IP', async t => {
+    const app = fastify()
+
+    app.register(plugin)
+
+    app.get('/', (req, reply) => {
+      t.equal(req.inferIPVersion(faker.internet.ipv6()), 6)
+      t.equal(req.inferIPVersion(faker.internet.ipv4()), 4)
+      t.equal(req.inferIPVersion('zsdc'), 0)
+
+      reply.send('')
+    })
+
+    t.plan(3)
+
+    await app.inject({
+      path: '/'
+    })
+  })
+})
+
+tap.test('Plugin#Request IP', scope => {
+  scope.plan(6)
+
+  scope.test('Should infer the header based on default priority', async t => {
+    const app = fastify()
+    const expectedIP = faker.internet.ip()
+    const secondaryIP = faker.internet.ip()
+
+    app.register(plugin)
+
+    app.get('/', (req, reply) => {
+      t.equal(req.ip, expectedIP)
+      t.equal(req._fastifyip, expectedIP)
+
+      reply.send('')
+    })
+
+    t.plan(2)
+
+    await app.inject({
+      path: '/',
+      headers: {
+        'x-client-ip': expectedIP,
+        'cf-connecting-ip': secondaryIP
+      }
+    })
+  })
+
+  scope.test(
+    'Should infer the header based on custom priority <Array>',
+    async t => {
+      const app = fastify()
+      const expectedIP = faker.internet.ip()
+      const fallbackIP = faker.internet.ipv6()
+
+      app.register(plugin, { order: ['x-custom-remote-ip'] })
+
+      app.get('/', (req, reply) => {
+        t.equal(req.ip, expectedIP)
+        t.equal(req._fastifyip, expectedIP)
+
+        reply.send('')
+      })
+
+      app.get('/fallback', (req, reply) => {
+        t.equal(req.ip, fallbackIP)
+        t.equal(req._fastifyip, fallbackIP)
+
+        reply.send('')
+      })
+
+      t.plan(4)
+
+      await app.inject({
+        path: '/',
+        headers: {
+          'cf-connecting-ip': fallbackIP,
+          'x-client-ip': faker.internet.ipv6(),
+          'x-custom-remote-ip': expectedIP
+        }
+      })
+
+      await app.inject({
+        path: '/fallback',
+        headers: {
+          'cf-connecting-ip': fallbackIP
+        }
+      })
+    }
+  )
+
+  scope.test(
+    'Should infer the header based on custom priority <Array> (strict)',
+    async t => {
+      const app = fastify()
+      const expectedIP = faker.internet.ip()
+      const fallbackIP = faker.internet.ipv6()
+
+      app.register(plugin, { order: ['x-custom-remote-ip'], strict: true })
+
+      app.get('/', (req, reply) => {
+        t.equal(req.ip, expectedIP)
+        t.equal(req._fastifyip, expectedIP)
+
+        reply.send('')
+      })
+
+      app.get('/fallback', (req, reply) => {
+        t.equal(req.ip, '')
+        t.equal(req._fastifyip, '')
+
+        reply.send('')
+      })
+
+      t.plan(4)
+
+      await app.inject({
+        path: '/',
+        headers: {
+          'cf-connecting-ip': fallbackIP,
+          'x-client-ip': faker.internet.ipv6(),
+          'x-custom-remote-ip': expectedIP
+        }
+      })
+
+      await app.inject({
+        path: '/fallback',
+        headers: {
+          'x-client-ip': faker.internet.ipv6(),
+          'cf-connecting-ip': fallbackIP
+        }
+      })
+    }
+  )
+
+  scope.test(
+    'Should infer the header based on custom priority <String>',
+    async t => {
+      const app = fastify()
+      const expectedIP = faker.internet.ip()
+      const fallbackIP = faker.internet.ipv6()
+
+      app.register(plugin, { order: 'x-custom-remote-ip' })
+
+      app.get('/', (req, reply) => {
+        t.equal(req.ip, expectedIP)
+        t.equal(req._fastifyip, expectedIP)
+
+        reply.send('')
+      })
+
+      app.get('/fallback', (req, reply) => {
+        t.equal(req.ip, fallbackIP)
+        t.equal(req._fastifyip, fallbackIP)
+
+        reply.send('')
+      })
+
+      t.plan(4)
+
+      await app.inject({
+        path: '/',
+        headers: {
+          'cf-connecting-ip': fallbackIP,
+          'x-client-ip': faker.internet.ipv6(),
+          'x-custom-remote-ip': expectedIP
+        }
+      })
+
+      await app.inject({
+        path: '/fallback',
+        headers: {
+          'cf-connecting-ip': fallbackIP
+        }
+      })
+    }
+  )
+
+  scope.test(
+    'Should infer the header based on custom priority <String> (strict)',
+    async t => {
+      const app = fastify()
+      const expectedIP = faker.internet.ip()
+      const fallbackIP = faker.internet.ipv6()
+
+      app.register(plugin, { order: 'x-custom-remote-ip', strict: true })
+
+      app.get('/', (req, reply) => {
+        t.equal(req.ip, expectedIP)
+        t.equal(req._fastifyip, expectedIP)
+
+        reply.send('')
+      })
+
+      app.get('/fallback', (req, reply) => {
+        t.equal(req.ip, '')
+        t.equal(req._fastifyip, '')
+
+        reply.send('')
+      })
+
+      t.plan(4)
+
+      await app.inject({
+        path: '/',
+        headers: {
+          'cf-connecting-ip': fallbackIP,
+          'x-client-ip': faker.internet.ipv6(),
+          'x-custom-remote-ip': expectedIP
+        }
+      })
+
+      await app.inject({
+        path: '/fallback',
+        headers: {
+          'cf-connecting-ip': fallbackIP
+        }
+      })
+    }
+  )
+
+  scope.test(
+    'Should discard the IP if invalid format and fallback into the following in the order',
+    async t => {
+      const app = fastify()
+      const expectedIP = faker.internet.ip()
+      const fallbackIP = 'abc'
+
+      app.register(plugin)
+
+      app.get('/', (req, reply) => {
+        t.equal(req.ip, expectedIP)
+        t.equal(req._fastifyip, expectedIP)
+
+        reply.send('')
+      })
+
+      t.plan(2)
+
+      await app.inject({
+        path: '/',
+        headers: {
+          'x-appengine-user-ip': expectedIP,
+          'x-real-ip': fallbackIP
+        }
+      })
+    }
+  )
+})
